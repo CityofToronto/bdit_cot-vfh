7d4508b7c99f9a528ee9dccdc5876dfb55213733
katiRG <cnangini@gmail.com>
2019-10-10 09:59:34 -0400

    full path for i18n in i18n.load()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,96 +1,7 @@
 // import settingsTPDline from "./settings_tpdLineChart.js";
 // import settingsTOWline from "./settings_towLineChart.js";
 
-const settingsTPDline = {
-  // alt: i18next.t("alt", {ns: "towline"}),
-  margin: {
-    top: 5,
-    right: 30,
-    bottom: 20,
-    left: 125
-  },
-  aspectRatio: 16 / 3,
-  datatable: false,
-  filterData: function(d) {
-    const root = d.tow;
-    const keys = this.z.getKeys(root);
-    const skip = 6; // number of hours to skip in 24h; for x-axis ticks
-    let xtickIdx = root.keys.values.map((q) => {
-      if (q * skip <= 162) return q * skip;
-    });
-    xtickIdx = xtickIdx.filter((q)=> {
-      return q != undefined;
-    });
-    return keys.map(function(key) {
-      return {
-        id: key,
-        xtickIdx: xtickIdx,
-        values: root[key].map(function(value, index) {
-          return {
-            year: root.keys.values[index],
-            value: value
-          };
-        })
-      };
-    });
-  },
-  x: {
-    // label: i18next.t("x_label", {ns: "towline"}),
-    type: "linear",
-    getValue: function(d) {
-      // return new Date(d.year + "-01");
-      return d.year;
-    },
-    getText: function(d) {
-      return d.year;
-    },
-    // ticks: 28,
-    getTickText: function(val) {
-      const modVal = val % 24;
-      return modVal;
-    },
-    translateXY: [-380, 45]
-  },
-
-  y: {
-    // label: i18next.t("y_label", {ns: "towline"}),
-    getValue: function(d) {
-      return d.value;
-    },
-    getText: function(d) {
-      return Math.round(d.value);
-    },
-    translateXY: [-60, 95],
-    ticks: 2
-  },
-
-  z: {
-    // label: i18next.t("z_label", {ns: "towline"}),
-    getId: function(d) {
-      return d.id;
-    },
-    getKeys: function(d) {
-      const keys = Object.keys(d);
-      keys.splice(keys.indexOf("keys"), 1);
-      return keys;
-    },
-    getxtickIdx: function(filteredData) {
-      return filteredData.map((d) => {
-        return d.xtickIdx;
-      })[0];
-    },
-    getClass: function(...args) {
-      return this.z.getId.apply(this, args);
-    },
-    getDataPoints: function(d) {
-      return d.values;
-    },
-    getText: function(d) {
-      // return i18next.t(d.id, {ns: "towline"});
-    }
-  },
-  width: 900
-};
+// const settingsTPDline = {};
 // The main javascript file for bdit_cot-vfh.
 // IMPORTANT:
 // Any resources from this project should be referenced using SRC_PATH preprocessor var
@@ -198,7 +109,7 @@ $(function () {
   }
   // -----------------------------------------------------------------------------
   // Initial page load
-  i18n.load(["i18n"], () => {
+  i18n.load(["/webapps/bdit_cot-vfh/i18n"], () => {
     // settingsTPDline.y.label = i18next.t("y_label", {ns: "line"}),
     // settingsTPDline.x.label = i18next.t("x_label", {ns: "line"}),
     // settingsTOWline.x.label = i18next.t("x_label", {ns: "towline"}),
@@ -208,13 +119,13 @@ $(function () {
         .defer(d3.json, "data/fig2_dummy_ptc_AM_downtown.json") // time of day ts
         .defer(d3.json, "data/fig3_tow_profile_city.json") // time of week ts
         .await(function(error, tpdfile, tpdAMfile, towfile) {
-          ptcData[tpd] = tpdfile;
-          ptcData[tpdAM] = tpdAMfile;
-          ptcData[tow] = towfile;
+          // ptcData[tpd] = tpdfile;
+          // ptcData[tpdAM] = tpdAMfile;
+          // ptcData[tow] = towfile;
 
           pageTexts();
-          showtpdLine();
-          showtowLine();
+          // showtpdLine();
+          // showtowLine();
         });
   });
 }); // function()
