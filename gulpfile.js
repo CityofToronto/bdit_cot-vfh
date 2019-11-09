5fcc799ed785cc70eb9a5e4dcbd166e91308c118
katiRG <cnangini@gmail.com>
2019-11-08 23:44:05 -0500

    init files from gulp scaffold for embedded apps

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const core = require('./node_modules/core/gulp_helper');
 const pkg = require('./package.json');
-const dir = '/resources';
 
 core.embeddedApp.createTasks(gulp, {
   pkg,
@@ -15,34 +14,3 @@ core.embeddedApp.createTasks(gulp, {
     prod: {}
   }
 });
-gulp.task('_data', () => {
- let cotuiDEVPath = '/resources/cdn/cotui';
- gulp.src(['node_modules/cotui/dist/**/*']).pipe(gulp.dest('dist' + cotuiDEVPath));
-});
-
-
-gulp.task('lib', () => {
-  console.log('adding lib dir');
-  return gulp.src(['src/lib/**/*'])
-    .pipe(gulp.dest('dist' + dir + '/lib'));
-});
-
-gulp.task('i18n', () => {
-  console.log('adding i18n dir');
-  return gulp.src(['src/i18n/*.json'])
-    .pipe(gulp.dest('dist' + dir + '/i18n'));
-});
-
-gulp.task('settings', () => {
-  console.log('adding settings*.js files');
-  return gulp.src(['src/scripts/settings*.js'])
-    .pipe(gulp.dest('dist' + dir + '/scripts'));
-});
-
-gulp.task('data', () => {
-  console.log('adding data files');
-  return gulp.src(['src/data/*.json'])
-    .pipe(gulp.dest('dist' + dir + '/data'));
-});
-
-gulp.task('build', ['_html_styles_scripts', '_images', '_fonts', '_extras', '_bower_extras', '_data', 'lib', 'i18n', 'settings', 'data']);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,67 +13,9 @@ $(function () {
       searchcontext: 'INTER'
     });
 
-    // COMMENT OUT WHEN NOT ON CITY NETWORK
-    // app.setBreadcrumb([
-    //   {"name": "bdit_cot-vfh", "link": "#"}
-    // ]).render();
+    app.setBreadcrumb([
+      {"name": "bdit_cot-vfh", "link": "#"}
+    ]).render();
   }
   let container = $('#bdit_cot-vfh_container');
-
 });
-
-
-// -----------------------------------------------------------------------------
-// settings
-// let settingsTPDline;
-
-// data objects
-const ptcFraction = {}; //PTC Trip Fraction by ward
-const pudoMap = {}; //PUDO map by ward
-
-// data selectors
-const tpd = "tpd"; // trips per day
-const tpdAM = "tpdAM"; // trips per day AM
-const tow = "tow"; // time of week
-const ward = 1;
-
-// -----------------------------------------------------------------------------
-// Chart SVGs
-
-// Fig 4a - Trip Fraction line chart
-
-
-// -----------------------------------------------------------------------------
-// Tooltip divs
-
-
-// -----------------------------------------------------------------------------
-// Page texts
-function pageTexts() {
-  // Intro texts
-  d3.select(".page-header h1").text(i18next.t("pagetitle", {ns: "indexhtml"}));
-  d3.select("#introp").html(i18next.t("introp", {ns: "indexhtml"}));
-}
-
-// -----------------------------------------------------------------------------
-// Charts
-
-
-
-$(document).ready(function(){
-  // -----------------------------------------------------------------------------
-  // Initial page load
-  i18n.load(["/resources/i18n"], () => {
-    d3.queue()
-      .defer(d3.json, "/webapps/bdit_cot-vfh/data/fig4a_dummy_tripfraction_w22.json") // trip fraction for ward 22
-      .defer(d3.json, "/webapps/bdit_cot-vfh/data/fig4b_dummy_pudoMap_w22.json") // pudo map ward 22
-      .await(function(error,ptcfractionfile, pudomapfile) {
-        // Load data files into objects
-        ptcFraction[ward] = ptcfractionfile;
-        pudoMap[ward] = pudomapfile;
-
-        // Display texts
-        pageTexts();
-    });
-  })
-})
